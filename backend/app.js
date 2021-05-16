3b76c6d43ebd318d3b0774154e2c29124892cad9
Ramjonchhen <ramjonchen@gmail.com>
2021-05-16 19:40:21 +0545

    API made for login

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@ const app = express();
 const authrouter = require("./routes/authRoutes");
 const fileupload = require("express-fileupload");
 const port = process.env.PORT || 4040;
-app.use(express.json({limit: '100MB'}));
+app.use(express.json({ limit: "100MB" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(fileupload());
 
 app.use(authrouter);
 
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 app.listen(port, () => console.log("Hello from " + port));
