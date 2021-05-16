c23e26e292b0fd1f11ee6e7841a5c30a31593e80
bibhukiju <bibhukiju@gmail.com>
2021-05-16 18:22:52 +0545

    frontend is setup

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,4 +2,5 @@ const express = require("express");
 const authRouter = express.Router();
 const authController = require("../controller/auth");
 authRouter.post("/auth/register", authController.register);
+
 module.exports = authRouter;
